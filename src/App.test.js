a7187fc9798fa33399a07e8990f95e4522cea6e2
Abdul Shaikh <abdul.shaikh@qless.com>
2020-04-20 09:35:23 -0700

    Reintroduce tests and add basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders the parent container', () => {
+  const { container } = render(<App />);
+  expect(container).toBeInTheDocument();
+});
+
+test('Checks if the parent has class container', () => {
+  const { container } = render(<App />);
+  expect(container.firstChild).toHaveClass('container');
 });
